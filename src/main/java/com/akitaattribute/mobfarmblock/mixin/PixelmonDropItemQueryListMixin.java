3a9955ebaf904552f385c4025edbc4dd2b237ce7
AkitaAttribute <74178046+AkitaAttribute@users.noreply.github.com>
2026-07-15 00:58:55 -0400

    GPT-5.5 - Target Pixelmon drop query registry package

--- src/main/java/com/akitaattribute/mobfarmblock/mixin/PixelmonDropItemQueryListMixin.java
+++ src/main/java/com/akitaattribute/mobfarmblock/mixin/PixelmonDropItemQueryListMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.server.level.ServerPlayer;
 
-@Mixin(targets = "com.pixelmonmod.pixelmon.api.drops.DropItemQueryList", remap = false)
+@Mixin(targets = "com.pixelmonmod.pixelmon.entities.npcs.registry.DropItemQueryList", remap = false)
 public abstract class PixelmonDropItemQueryListMixin {
     static {
         MobFarmBlockMod.LOGGER.info("Mob Farm Pixelmon DropItemQueryList mixin class loaded");
