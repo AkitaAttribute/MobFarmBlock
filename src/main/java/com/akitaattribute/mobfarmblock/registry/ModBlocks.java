d4ab813df998b18acc83fa58381af1bc36224d18
AkitaAttribute <74178046+AkitaAttribute@users.noreply.github.com>
2026-07-22 04:06:34 -0400

    GPT-5.5 - Use named mob pen item

--- src/main/java/com/akitaattribute/mobfarmblock/registry/ModBlocks.java
+++ src/main/java/com/akitaattribute/mobfarmblock/registry/ModBlocks.java
@@ -3,9 +3,9 @@ package com.akitaattribute.mobfarmblock.registry;
 import com.akitaattribute.mobfarmblock.MobFarmBlockMod;
 import com.akitaattribute.mobfarmblock.block.MobFarmBlock;
 import com.akitaattribute.mobfarmblock.block.MobFarmBlockEntity;
+import com.akitaattribute.mobfarmblock.item.MobFarmBlockItem;
 
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
@@ -23,9 +23,9 @@ public final class ModBlocks {
             () -> new MobFarmBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.DIRT).strength(0.5F, 0.5F))
     );
 
-    public static final DeferredHolder<Item, BlockItem> MOB_FARM_BLOCK_ITEM = ModItems.ITEMS.register(
+    public static final DeferredHolder<Item, MobFarmBlockItem> MOB_FARM_BLOCK_ITEM = ModItems.ITEMS.register(
             "mob_farm_block",
-            () -> new BlockItem(MOB_FARM_BLOCK.get(), new Item.Properties())
+            () -> new MobFarmBlockItem(MOB_FARM_BLOCK.get(), new Item.Properties())
     );
 
     public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<MobFarmBlockEntity>> MOB_FARM_BLOCK_ENTITY = BLOCK_ENTITY_TYPES.register(
